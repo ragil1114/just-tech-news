f9437e90e273c4400608bc1edbaf94d44280b364
Ragil14 <rahil.gilani@gmail.com>
2022-02-07 15:20:49 -0600

    added .findAll() Method to GET API

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,7 +2,15 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // GET /api/users
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+  // Access our User model and run .findAll() method)
+  User.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 // GET /api/users/1
 router.get('/:id', (req, res) => {});
