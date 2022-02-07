5a7b8480aa59769cfb7fe9f1d6096654e2844793
Ragil14 <rahil.gilani@gmail.com>
2022-02-07 15:21:06 -0600

    insert comment

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,7 +3,7 @@ const { User } = require('../../models');
 
 // GET /api/users
 router.get('/', (req, res) => {
-  // Access our User model and run .findAll() method)
+  // Access our User model and run .findAll() method
   User.findAll()
     .then(dbUserData => res.json(dbUserData))
     .catch(err => {
