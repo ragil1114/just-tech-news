b2a54ac1592d95100298e9fa51a3e3055ce2b590
Ragil14 <rahil.gilani@gmail.com>
2022-02-07 15:37:58 -0600

    added POST Route to create a user

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -34,7 +34,19 @@ router.get('/:id', (req, res) => {
 });
 
 // POST /api/users
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {});
