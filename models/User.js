7523aeaa601f51eac0c8042d0c09e5238e4a3b1e
Ragil14 <rahil.gilani@gmail.com>
2022-02-08 14:48:14 -0600

    insert comment

--- models/User.js
+++ models/User.js
@@ -47,7 +47,7 @@ User.init(
     },
   {
     hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
+      // set up beforeCreate lifecycle "hook" functionality. Async hooks function inside the hooks object property.
       beforeCreate(userData) {
         return bcrypt.hash(userData.password, 10).then(newUserData => {
           return newUserData
