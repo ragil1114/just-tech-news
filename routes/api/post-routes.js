efd93ed1cd36e72ffe4aad246ea9700e00aaba85
Ragil14 <rahil.gilani@gmail.com>
2022-02-09 16:04:11 -0600

    insert comment

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -14,6 +14,7 @@ router.get('/', (req, res) => {
         }
       ]
     })
+      // Promise that captures the response from the db Call
       .then(dbPostData => res.json(dbPostData))
       .catch(err => {
         console.log(err);
