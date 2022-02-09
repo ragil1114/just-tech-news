614098a44d7b2a25039f9b70e66994bb4e876f40
Ragil14 <rahil.gilani@gmail.com>
2022-02-09 16:36:20 -0600

    added API Route to update a post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -64,4 +64,29 @@ router.post('/', (req, res) => {
       });
 });
 
+// Update a post
+router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
   module.exports = router;
