aab108ee93e8f1d7fbc72defd5964a0d8ab78942
Ragil14 <rahil.gilani@gmail.com>
2022-02-09 15:48:10 -0600

    added API Route to GET all posts from db

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,2 +1,11 @@
 const router = require('express').Router();
 const { Post, User } = require('../../models');
+
+// get all posts in db
+router.get('/', (req, res) => {
+    console.log('======================');
+    Post.findAll({
+      // Query configuration
+    })
+  
+  });
