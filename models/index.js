b242f22026dc986394cf7f4bb95b6575a62052eb
Ragil14 <rahil.gilani@gmail.com>
2022-02-10 13:30:34 -0600

    added associations to directly connect the vote to user & post models

--- models/index.js
+++ models/index.js
@@ -24,4 +24,20 @@ Post.belongsToMany(User, {
     foreignKey: 'post_id'
 });
 
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
 module.exports = { User, Post };
