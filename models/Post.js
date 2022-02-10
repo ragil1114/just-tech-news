16944caf3e4fffb9723c555e45724350c4eca10e
Ragil14 <rahil.gilani@gmail.com>
2022-02-10 15:35:20 -0600

    added static keyword to indicate the upvote method is based on the Post Model

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,9 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our Post model
-class Post extends Model {}
+class Post extends Model {
+  static upvote(body, models) {}
+}
 
 // create fields/columns for Post model
 Post.init(
