e5dfc37a1a6e2d926f565e58b3dd0f29a5638ea2
Ragil14 <rahil.gilani@gmail.com>
2022-02-10 13:48:49 -0600

    imported vote model in post-routes.js file

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
 
 // get all posts in db
 router.get('/', (req, res) => {
