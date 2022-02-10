1c5b3771195bf0f5a85324419360b372c6722425
Ragil14 <rahil.gilani@gmail.com>
2022-02-10 14:42:40 -0600

    imported special Sequelize functionality in post-routes.js file. (took it out of server.js file where it was accidentally placed)

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
 const { Post, User, Vote } = require('../../models');
 
 // get all posts in db
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
-const sequelize = require('../../config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
