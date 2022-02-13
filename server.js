3c675dc8979843efafb169d4af6176be7bc8b2b3
Ragil14 <rahil.gilani@gmail.com>
2022-02-13 14:32:47 -0600

    added Express.js Middleware function using express.static() Method

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
@@ -7,6 +8,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// The express.static() method is a built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets. 
+// This is useful for front-end specific files like images, style sheets, and JavaScript files.
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
