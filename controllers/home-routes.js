308b2c797e8dd0e675866d7cf6e7630f2283d6dc
Ragil14 <rahil.gilani@gmail.com>
2022-02-13 16:13:05 -0600

    import modules & models

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,10 @@
 // This file will contain all of the user-facing routes, such as the homepage and login page.
 
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
+// Homepage route
 router.get('/', (req, res) => {
   res.render('homepage', {
     id: 1,
