178f22237f43b3dbff436019f520f6692c6626db
Ragil14 <rahil.gilani@gmail.com>
2022-02-15 16:53:48 -0600

    login and logout links will conditionally render based on the user's session

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -35,7 +35,10 @@ router.get('/', (req, res) => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
         // pass a single post object into the homepage template
         // console.log(dbPostData[0]);
-        res.render('homepage', { posts });
+        res.render('homepage', { 
+          posts,
+          loggedIn: req.session.loggedIn
+        });
       })
       .catch(err => {
         console.log(err);
