b470fa16dc32794bc91f9d2eb5ded959a8cc16dd
Ragil14 <rahil.gilani@gmail.com>
2022-02-14 19:54:24 -0600

    add if conditional to check for a session and redirect to the homepage if one exists

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -6,6 +6,7 @@ const { Post, User, Comment } = require('../models');
 
 // Homepage route to get all posts
 router.get('/', (req, res) => {
+  console.log(req.session);
     Post.findAll({
       attributes: [
         'id',
@@ -44,6 +45,11 @@ router.get('/', (req, res) => {
 
 // Route to render login page
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
 });
 
