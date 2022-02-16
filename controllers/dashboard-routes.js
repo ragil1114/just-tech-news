355aff5c773eb81b5a1371ae10d435a50f74d122
Ragil14 <rahil.gilani@gmail.com>
2022-02-16 14:13:38 -0600

    imported auth.js to dashboard-routes.js file to allow Authguard of dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 // Route for main Dashboard page
 router.get('/', (req, res) => {
