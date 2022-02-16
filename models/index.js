cd851c8385521fb10602d38b9b6ec6f9d38f71b9
Ragil14 <rahil.gilani@gmail.com>
2022-02-16 01:08:34 -0600

    clean code

--- models/index.js
+++ models/index.js
@@ -1,62 +1,68 @@
-const User = require('./User');
+// import all models
 const Post = require('./Post');
+const User = require('./User');
 const Vote = require('./Vote');
 const Comment = require('./Comment');
 
-// User & Post Model Associations
+// create associations
 User.hasMany(Post, {
-    foreignKey: 'user_id'
+  foreignKey: 'user_id'
 });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id',
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
 });
 
-// These Associations allow models to query each other's information in the context of a vote.
 User.belongsToMany(Post, {
-    through: Vote,
-    as: 'voted_posts',
-    foreignKey: 'user_id'
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
 });
-  
+
 Post.belongsToMany(User, {
-    through: Vote,
-    as: 'voted_posts',
-    foreignKey: 'post_id'
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
 });
 
-// These Associations directly connect the Post, User & Vote models.
 Vote.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-  
-Vote.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
-  
-User.hasMany(Vote, {
-    foreignKey: 'user_id'
-});
-  
-Post.hasMany(Vote, {
-    foreignKey: 'post_id'
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
 });
 
-// Associations for Comment Model
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
 });
-  
+
 Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
+  foreignKey: 'post_id',
+  onDelete: 'SET NULL'
 });
-  
+
 User.hasMany(Comment, {
-    foreignKey: 'user_id'
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
 });
-  
+
 Post.hasMany(Comment, {
-    foreignKey: 'post_id'
+  foreignKey: 'post_id'
 });
 
 module.exports = { User, Post, Vote, Comment };
